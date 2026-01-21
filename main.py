a63c22b5c9d094694500692df6655341f988f513
rabia3007 <abdukhadirovarobiya@gmail.com>
2026-01-21 15:09:14 +0500

    Perviy commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from project380robiya import robiya
+
+n = int(input("Введите целое число: "))
+print(robiya(n))
---
+++ project380robiya.py
@@ -0,0 +1,8 @@
+def robiya(x):
+    if x > 0:
+        x += 1
+    elif x < 0:
+        x -= 2
+    else:
+        x = 10
+    return x
